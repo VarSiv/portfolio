272bfcc367582ca47a8d1b49fb8d81808eab299f
Siv <var.mironov@gmail.com>
2024-09-05 14:22:07 -0300

    contact form functionality done

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,12 +1,18 @@
-import React, { useRef, useEffect } from 'react';
-import { Flex, VStack, Heading, Text, Button, Image, Box } from '@chakra-ui/react'
+import React, { useRef, useEffect, useState } from 'react';
+
+import { Flex, VStack, Heading, Text, Button, Image, Box,  
+    FormControl,
+    FormLabel,
+    FormErrorMessage,
+    FormHelperText, Input} from '@chakra-ui/react';
+
 import translations from '../translations.json';
 import siam from '../images/siam.png';
 import { LINE_BLUE, TEXT_BLUE } from "../App";
 import { TextButton } from "./navbar";
 import emailjs from '@emailjs/browser';
 export const Contact = ({ language }) => {
-
+    const [formData, setFormData] = useState({ email: '', body: '' });
     useEffect(() => {
         emailjs.init("V5Jrx1x7PO44Y11XH");
       }, []);
@@ -14,38 +20,57 @@ export const Contact = ({ language }) => {
     const getTranslation = (key) => {
         return translations[language]?.[key] || key;
     };
+
     const sendEmail = () => {
         const templateParams = {
-            // from_name: form.current.name.value,
-            // from_email: form.current.email.value,
-            // message: form.current.message.value,
-            // to_name: 'Your Name', 
-            from_name: 'me',
-            from_email: 'also me',
-            message: 'test msg hello world',
+            from_name: formData.email,
+            from_email: formData.email,
+            message: formData.body,
             to_name: 'Your Name', 
-          };
+        };
 
-        emailjs.send("service_arx73hw", "template_t9byr35", templateParams,"V5Jrx1x7PO44Y11XH",{
-          to_name: "Recipient Name",
-          from_name: "Your Name",
-          message: "This is a test email"
-        })
+        emailjs.send("service_arx73hw", "template_t9byr35", templateParams,"V5Jrx1x7PO44Y11XH")
         .then((response) => {
-           console.log('SUCCESS!', response.status, response.text);
+            console.log('SUCCESS!', response.status, response.text);
         }, (err) => {
-           console.log('FAILED...', err);
+            console.log('FAILED...', err);
         });
-        console.log('clicked');
-      };
+    };
+
+      const handleInputChange = (e) => {
+        const { name, value } = e.target;
+        setFormData(prevData => ({ ...prevData, [name]: value }));
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        sendEmail();
+    };
       
     return (    
         <Flex maxW="6xl" w="full" flexDir={'row'} alignItems="center" justifyContent="space-between" mx="auto" marginTop={100}>
             
             <VStack width='50%' height='50%' p={10} alignItems="left" alignSelf="center">
-                <Button onClick={sendEmail()}>
-                    Send
-                </Button>
+                <form onSubmit={handleSubmit}>
+                <FormControl>
+                <Input 
+                    
+                    type='email' 
+                    name='email' 
+                    value={formData.email} 
+                    onChange={handleInputChange}
+                    color="white"
+                />
+                <Input 
+                    type='text' 
+                    name='body' 
+                    value={formData.body} 
+                    onChange={handleInputChange}
+                    color="white"
+                />
+            </FormControl>
+            <Button type="submit">Send</Button>
+                </form>
             </VStack>
             <VStack width='50%' height='50%' p={10} alignItems="left" alignSelf="center">
                 <Text fontSize={"6xl"} fontWeight={'semibold'} color="white" className="font-rubik" paddingBottom={2} marginBottom={0}>
